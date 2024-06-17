85e2fe15c506751897fe38efb944569f74b429f6
icabellos <ivancabe.96@gmail.com>
2024-06-17 14:59:28 +0200

    added admin

--- IDEL_web_project/frontend/models.py
+++ IDEL_web_project/frontend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='cards-img/')
+    url_to_jatos = models.URLField(max_length=200, default='')
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- IDEL_web_project/frontend/views.py
+++ IDEL_web_project/frontend/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from .models import Project
 
 
 class IndexView(TemplateView):
@@ -6,8 +7,18 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['projects'] = Project.objects.filter(is_active=True)
 
-        context['cards'] = [
+        return context
+
+
+class StatisticView(TemplateView):
+    template_name = "statistic.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['statistics'] = [
             {'title': 'Card 1', 'description': 'Description for card 1'},
             {'title': 'Card 2', 'description': 'Description for card 2'},
             {'title': 'Card 3', 'description': 'Description for card 3'},
