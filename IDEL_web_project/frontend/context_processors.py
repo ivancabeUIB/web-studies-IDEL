cd0e3a64c8d78dd64402f34981de759040bb961e
Ivan <i.cabellos@id.uib.eu>
2025-01-22 09:20:34 +0100

    testing

--- IDEL_web_project/frontend/context_processors.py
+++ IDEL_web_project/frontend/context_processors.py
@@ -2,10 +2,10 @@ from .models import ImageFavicon, FooterBanner
 
 
 def favicon_context(request):
-    favicon = ImageFavicon.objects.all().first() if ImageFavicon.objects.all().exists() else ''
+    #favicon = ImageFavicon.objects.all().first() if ImageFavicon.objects.all().exists() else ''
     footer = FooterBanner.objects.all().first() if FooterBanner.objects.all().exists() else []
 
     return {
-        'favicon': favicon,
+        #'favicon': favicon,
         'footer': footer,
     }
