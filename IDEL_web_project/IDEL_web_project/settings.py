05175e1ca6bde704421291e7325d9baa807886a8
Ivan <i.cabellos@id.uib.eu>
2025-01-27 12:27:21 +0100

    fixed some issues with deployment

--- IDEL_web_project/IDEL_web_project/settings.py
+++ IDEL_web_project/IDEL_web_project/settings.py
@@ -142,10 +142,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if os.getenv("STATIC_ROOT"):
+    STATIC_ROOT = os.getenv("STATIC_ROOT")
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
